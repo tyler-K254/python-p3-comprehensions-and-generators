9c2069f45415d67cf8058ed43da06528fb24a2a3
tylerkirumba <Tyler>
2023-05-20 19:51:30 +0300

    Completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list =[n for n in num_list if n%2 ==0]
+    return(even_list)
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation = [(n+"!") for n in sentence_list]
+    return(exclamation)
---
+++ practice.py
@@ -0,0 +1,8 @@
+for n in range(1,11):
+    squared_minus_one.append((n**2)-1)
+    print(squared_minus_one)
+
+squared_minus_on =[(n**2) -1 for n in range(1,11)]
+
+print(squared_minus_on)
+
